942052baeab832c0f158f421a7ce5a38cb7ba323
mrcobranza2025 <mrcobranza2025@gmail.com>
2025-11-24 18:14:43 -0300

    fix firestore db

--- src/routes/users/users.py
+++ src/routes/users/users.py
@@ -3,9 +3,9 @@ from src.core.firebase import verify_firebase_token
 from src.services.firestore_service import FirestoreService
 
 router = APIRouter()
-service = FirestoreService()
 
 @router.get("/")
 def list_users(decoded = Depends(verify_firebase_token)):
+    service = FirestoreService()
     # return service.get_all("users")
     return ''
